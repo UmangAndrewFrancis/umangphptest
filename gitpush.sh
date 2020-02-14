c840640a7ffb058b2f7157086c262b8e6e1fe8ac
umangandrewfrancis <umangandrewfrancis@gmail.com>
2020-02-14 17:08:35 +0000

    new commit

--- gitpush.sh
+++ gitpush.sh
@@ -1,19 +1,9 @@
 git config --global user.email "umangandrewfrancis@gmail.com"
-
 git config --global user.name "umangandrewfrancis"
-
-
-
 git init /home/site/wwwroot
-
 cd /home/site/wwwroot ; git add .
-
 cd /home/site/wwwroot ; git commit -m "new commit"
-
 set +e
-
-cd /home/site/wwwroot ; git remote add origin https://umangandrewfrancis:andrewassassin32@github.com/UmangAndrewFrancis/umangphptest.git`
-
+cd /home/site/wwwroot ; git remote add origin https://umangandrewfrancis:andrewassassin32@github.com/UmangAndrewFrancis/umangphptest.git
 set -e
-
 cd /home/site/wwwroot ; git push prigin master
